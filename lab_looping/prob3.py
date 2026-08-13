4710de396b64525f44708c1228b6fd7c41dd8652
pringkay12 <leniprincess122gmail.com>
2026-08-13 16:14:38 +0800

    add prob5.py file

--- lab_looping/prob3.py
+++ lab_looping/prob3.py
@@ -1,7 +1,7 @@
 n = int(input("Enter n:"))
 multiple = int(input("Enter multiple:"))
 total_sum = 0
-for number in range(1, n + 1)
-if number % multiple == 0:
-    total_sum += 0:
+for number in range(1, n + 1):
+    if number % multiple == 0:
+        total_sum += 0
 print(total_sum)
