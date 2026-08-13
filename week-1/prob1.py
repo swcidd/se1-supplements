7e3da921ae63c4e0da02f8e4ec7c6522e847f6db
GillianZyra <gillianzyra@gmail.com>
2026-08-13 18:18:37 +0800

    add prob1.py temp code runner py prob2.py

--- week-1/prob1.py
+++ week-1/prob1.py
@@ -1,3 +1,16 @@
-print("Cute ako")
+#Put your input logic here
+string = input("Enter a string: ")
+new_string = ""
+vowels = "aeiou"
+#Put your for loop logic here
+for letter in string:
 
-print("SE1-Supplements")
+    if letter.lower() in "aeiou":
+        new_string += "*"
+    else:
+        new_string += letter
+
+print(f"{new_string} Kla")
+print("Hello, {new_string}!")
+print("Hello," + new_string)
+print(new_string) 
