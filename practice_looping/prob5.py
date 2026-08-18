eed9cf3403243ed0750dd8c5f839e5794f3e60af
pringkay12 <leniprincess122gmail.com>
2026-08-18 19:41:53 +0800

    add prob5.py file

--- practice_looping/prob5.py
+++ practice_looping/prob5.py
@@ -1,3 +1,8 @@
+##Problem 1: Right-Angled Number Pyramid (Easy)
+##Write a Python program that takes an integer N and prints a right-angled triangle where row i
+#contains the number i repeated i times.
+#Input: N = 5
+
 n = int(input("Enter a number:"))
 for n in range(1, n + 1):
     for i in range(1, n):
